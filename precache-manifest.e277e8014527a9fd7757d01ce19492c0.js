9acedefe76e57f849c9d2c4cc3e272a568076b1a
shambhavibudholiya <shambhavi20.1998@gmail.com>
2020-05-04 11:38:49 +0530

    Updates

---
+++ precache-manifest.e277e8014527a9fd7757d01ce19492c0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa198f816001c41a75e634ded8c8bd8",
+    "url": "/dad-website/index.html"
+  },
+  {
+    "revision": "cb64f4a803b6c5434d04",
+    "url": "/dad-website/static/css/2.5386b619.chunk.css"
+  },
+  {
+    "revision": "01144e5be092aa4a4e5c",
+    "url": "/dad-website/static/css/main.8a218122.chunk.css"
+  },
+  {
+    "revision": "cb64f4a803b6c5434d04",
+    "url": "/dad-website/static/js/2.8690fbcb.chunk.js"
+  },
+  {
+    "revision": "73d107ee83c6947ee29fd47930ccb2aa",
+    "url": "/dad-website/static/js/2.8690fbcb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01144e5be092aa4a4e5c",
+    "url": "/dad-website/static/js/main.7a60b424.chunk.js"
+  },
+  {
+    "revision": "9443ccbf61854cc546e6",
+    "url": "/dad-website/static/js/runtime-main.bf4cd83d.js"
+  },
+  {
+    "revision": "76bbf4f1f1d59769d2cf5eacf5754da1",
+    "url": "/dad-website/static/media/1.76bbf4f1.jpg"
+  },
+  {
+    "revision": "c1eeff0f122f3db457cd9318c9d933db",
+    "url": "/dad-website/static/media/10.c1eeff0f.jpg"
+  },
+  {
+    "revision": "cc7eac8d7b24e367e5ef0eca6d4e4b6b",
+    "url": "/dad-website/static/media/11.cc7eac8d.jpg"
+  },
+  {
+    "revision": "61e188c5ae25f12fe1ebe3fa6448790a",
+    "url": "/dad-website/static/media/12.61e188c5.jpg"
+  },
+  {
+    "revision": "dd421e3fa1680ffabb05662c9a684209",
+    "url": "/dad-website/static/media/13.dd421e3f.jpg"
+  },
+  {
+    "revision": "f6b8f65f23ac7caa4dcd5fb6826487f1",
+    "url": "/dad-website/static/media/14.f6b8f65f.JPG"
+  },
+  {
+    "revision": "4aac5fa27dc902ee018520d3bf7ed2f7",
+    "url": "/dad-website/static/media/15.4aac5fa2.JPG"
+  },
+  {
+    "revision": "adb02afaa2982ef5b9a1d33cd1379ead",
+    "url": "/dad-website/static/media/17.adb02afa.jpg"
+  },
+  {
+    "revision": "c54092bfbad46f1d2729e87813c7964d",
+    "url": "/dad-website/static/media/18.c54092bf.jpg"
+  },
+  {
+    "revision": "36cd5260c806f66391992cf8943d65b5",
+    "url": "/dad-website/static/media/19.36cd5260.jpg"
+  },
+  {
+    "revision": "fdaeca7aebb980cbc1c27028a6eea1ca",
+    "url": "/dad-website/static/media/4.fdaeca7a.jpg"
+  },
+  {
+    "revision": "0a715660892b03b32a4dc236b34c8944",
+    "url": "/dad-website/static/media/5.0a715660.JPG"
+  },
+  {
+    "revision": "7afbfe2828568df961a4d8f26709bb14",
+    "url": "/dad-website/static/media/6.7afbfe28.jpg"
+  },
+  {
+    "revision": "1f1c6de1bf5b0be9027c198c2626f246",
+    "url": "/dad-website/static/media/7.1f1c6de1.JPG"
+  },
+  {
+    "revision": "554d5e65161b054b5c9df85ae30d4569",
+    "url": "/dad-website/static/media/8.554d5e65.JPG"
+  },
+  {
+    "revision": "558a4f3350bd56cf71ea76e8e6da8152",
+    "url": "/dad-website/static/media/9.558a4f33.jpg"
+  }
+]);
